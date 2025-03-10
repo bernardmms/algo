834ccd8602c4c0a3bef1668de3711169aa60ce41
bernardmms <bernardmouras@gmail.com>
2025-03-10 18:27:35 -0300

    Vetor Esparso

--- VetorEsparso/src/VetorEsparso.cpp
+++ VetorEsparso/src/VetorEsparso.cpp
@@ -30,3 +30,26 @@ int VetorEsparso::detInd(int i)
         exit(1);
     }
 }
+
+float VetorEsparso::get(int i)
+{
+    int k = detInd(i);
+    if(k != -1) return tab[k].val;
+    else return 0;
+}
+
+void VetorEsparso::setInd(int i, float valor)
+{
+    int k = detInd(i);
+    if(k != -1) tab[k].val = valor;
+    else
+    {
+        if(nnz < max)
+        {
+            tab[nnz].ind = i;
+            tab[nnz].val = valor;
+            nnz++;
+        }
+        else cout << "Nao ha espaco no vetor!" << endl;
+    }
+}
